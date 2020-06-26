3ed2b6c16af506dc990999bbec83bf5d14c87af5
Manish <manishej004@gmail.com>
2020-06-26 14:12:49 +0800

    first commit

---
+++ NavigationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationSwiftUI
+//
+//  Created by Manish Bhande on 26/06/20.
+//  Copyright © 2020 Manish Bhande. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
