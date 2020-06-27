259cb2c90ab549e617e142381254cb1db8ba107b
Manish <manishej004@gmail.com>
2020-06-27 20:32:17 +0800

    Update Package file.

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "NavigationModel",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "NavigationModel",
-            targets: ["NavigationModel"]),
-    ],
+//    products: [
+//        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+//        .library(
+//            name: "NavigationModel",
+//            targets: ["NavigationModel"]),
+//    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
